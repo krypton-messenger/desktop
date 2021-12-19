6383005a7de6875fd265b82f28dac5b502cdaaa7
ttschnz <tim@mw3.ch>
2021-12-19 09:33:23 +0100

    build test for darvin

--- src/build.js
+++ src/build.js
@@ -1,8 +1,16 @@
-const packager = require('electron-packager')
+const packager = require('electron-packager'),
+    path = require("path"), fs = require("fs");
 
-async function bundleElectronApp(options) {
+    
+    async function bundleElectronApp(options) {
+    fs.mkdirSync(options.dir, { recursive: true });
     const appPaths = await packager(options)
     console.log(`Electron app bundles created:\n${appPaths.join("\n")}`)
 }
 
-bundleElectronApp(all = true);
+bundleElectronApp({
+    platform: "darwin",
+    arch: "x64",
+    dir: process.cwd(),
+    out: path.resolve(process.cwd(), "builds", "darvin", String(new Date().getTime()))
+});
